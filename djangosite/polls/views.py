f14dd2e6389a736b62e5216d15600860d990ab7f
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2023-01-30 13:41:39 +0700

    Daily update progress 2023-01-30

---
+++ djangosite/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
